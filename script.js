e7d6f3b72b93544ae921730290a0a85269b096b9
Fortwin <basirathamz@gmail.com>
2022-06-10 11:09:09 +0100

    fixed navbar

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const navigation = document.getElementById("navbar");
-const menu = document.getElementById("menu");
-console.log(navigation)
-menu.addEventListener("click", () => {
+// const navigation = document.getElementById("navbar");
+// const menu = document.getElementById("menu");
+// console.log(navigation)
+// menu.addEventListener("click", () => {
+//   navigation.classList.add("active");
+//   menu.classList.add("active");
+// });
 
-  // navigation.style.setProperty("--childenNumber", navigation.children.length);
-  navigation.classList.add("active");
-  menu.classList.add("active");
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
 });
+
+
+navLink.addEventListener('click',() => {
+  navLink.innerHTML.toggle('hide');
+} )
