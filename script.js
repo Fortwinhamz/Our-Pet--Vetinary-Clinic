89ae83ec16b88f15ce057ae0195a16ea1c912a69
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2022-06-11 14:25:11 +0100

    rebuilt everything from scratch

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-// const navigation = document.getElementById("navbar");
-// const menu = document.getElementById("menu");
-// console.log(navigation)
-// menu.addEventListener("click", () => {
-//   navigation.classList.add("active");
-//   menu.classList.add("active");
-// });
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav a');
+const menuBar = document.querySelector('.toggle-menu');
 
+// function to show the whole nav when the nav bar is clicked
+const showNavMenu = () => {
+  nav.classList.toggle('showNav');
+  let closeBar = menuBar.querySelector('i');
+  closeBar.classList.toggle('showMenu');
+};
 
+// this fucntion is showing the active section when it is clicked on
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    const active = document.querySelector('.active');
+    active.classList.remove('active');
+    link.classList.add('active');
 
-
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
-
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
+    showNavMenu();
+  });
 });
 
-
-navLink.addEventListener('click',() => {
-  navLink.innerHTML.toggle('hide');
-} )
+// event listener to call the nav bar
+menuBar.addEventListener('click', showNavMenu);
